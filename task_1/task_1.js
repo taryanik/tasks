78175ff55137a149a27b30c6d34ef720f97841ca
tarianik <ukr.tda@gmail.com>
2020-12-25 13:50:20 +0200

    added 1 task

---
+++ task_1/task_1.js
@@ -0,0 +1,34 @@
+module.exports = (input) => {
+    let result = 0;
+    for (let i = 0; i < input.length; i++) {
+        let currentSymbol = getIntFromRoman(input[i]);
+
+        if ((i + 1) < input.length) {
+            let nextSymbol = getIntFromRoman(input[i + 1]);
+
+            if (currentSymbol >= nextSymbol) {
+                result = result + currentSymbol;
+            } else {
+                result = result + nextSymbol - currentSymbol;
+                i++;
+            }
+        } else {
+            result = result + currentSymbol;
+        }
+    }
+    return result;
+};
+
+let matches = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+function getIntFromRoman(roman) {
+    return matches[roman];
+}
---
+++ task_1/test_1.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+const resolve = require("./task_1.js");
+
+assert.strictEqual(resolve("III"), 3);
+assert.strictEqual(resolve("VI"), 6);
+assert.strictEqual(resolve("IX"), 9);
+assert.strictEqual(resolve("X"), 10);
+assert.strictEqual(resolve("LVIII"), 58);
+assert.strictEqual(resolve("MCMXCIV"), 1994);
